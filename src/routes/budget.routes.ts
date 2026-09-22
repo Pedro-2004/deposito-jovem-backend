ebf0546fa5765bbcdbb97b727d9250b520dd642f
Pedro Arthur <arthur.resende758@gmail.com>
2026-09-22 08:23:04 -0300

    configuration in rotes

--- src/routes/budget.routes.ts
+++ src/routes/budget.routes.ts
@@ -1,15 +1,25 @@
 import express from 'express';
-type User = {
+type BudgetBody = {
   productName: string;
+  clientName: string;
+  productQuantity: string;
 };
 
 const route = express.Router();
 
 route.post('/', (req, res) => {
   console.log(req.body);
-  const { productName } = req.body as User;
+  const { productName, clientName, productQuantity } = req.body as BudgetBody;
+  const quantity = Number(productQuantity);
 
-  return res.status(200).json({ message: 'Produto do orçamento', product: productName });
+  return res
+    .status(200)
+    .json({
+      message: 'Orçamento',
+      Produto: productName,
+      'Nome cliente': clientName,
+      'Quantidade do produto': quantity,
+    });
 });
 
 export default route;
