36b82e1e93a5703ee0218f10973dca76b67791c9
Pedro Arthur <arthur.resende758@gmail.com>
2026-09-25 21:42:46 -0300

    configuration of back-end

--- src/routes/budget.routes.ts
+++ src/routes/budget.routes.ts
@@ -1,25 +1,42 @@
 import express from 'express';
+import pool from '../config/database.js';
+
 type BudgetBody = {
   productName: string;
   clientName: string;
   productQuantity: string;
+  productValue: string;
 };
 
 const route = express.Router();
 
-route.post('/', (req, res) => {
+route.post('/', async (req, res) => {
   console.log(req.body);
-  const { productName, clientName, productQuantity } = req.body as BudgetBody;
+  const { productName, clientName, productQuantity, productValue } = req.body as BudgetBody;
   const quantity = Number(productQuantity);
+  const value = Number(productValue);
 
-  return res
-    .status(200)
-    .json({
-      message: 'Orçamento',
-      Produto: productName,
-      'Nome cliente': clientName,
-      'Quantidade do produto': quantity,
-    });
+  const totalValue = quantity * value;
+
+  const insertProductQuery = `INSERT INTO products(
+  client_name,
+  product_name,
+  product_quantity,
+  product_value,
+  total_value  
+  )
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *
+  `;
+
+  const values = [clientName, productName, quantity, value, totalValue];
+
+  const result = await pool.query(insertProductQuery, values);
+
+  return res.status(201).json({
+    message: 'Orçamento salvo com sucesso',
+    product: result.rows[0],
+  });
 });
 
 export default route;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
 import budgetroutes from './routes/budget.routes.js';
 import cors from 'cors';
-import pool from './config/database.js';
 
 const app = express();
 const PORT = 3000;
 
-const testDatabase = async () => {
-  const result = await pool.query('SELECT NOW()');
-  console.log(result.rows);
-};
-
-testDatabase();
 app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.use(express.json());
